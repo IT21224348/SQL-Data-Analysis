6e92c42bcb02e92887857884ef2e18442c37cc43
Ravindu Sulakkana <99631317+IT21224348@users.noreply.github.com>
2025-03-12 11:15:46 +0530

    Update project1.sql

--- Project 1/project1.sql
+++ Project 1/project1.sql
@@ -18,3 +18,48 @@ DATEDIFF(YEAR,MIN(birthdate),GETDATE()) AS 'Oldest Age',
 MAX(birthdate) AS 'Youngest Customer',
 DATEDIFF(YEAR, MAX(birthdate),GETDATE()) AS 'Youngest Age'
 FROM gold.dim_customers;
+
+--Find total sales
+SELECT SUM(sales_amount) AS 'Total Sales'
+FROM gold.fact_sales;
+
+--Find how many items are sold\
+SELECT SUM(quantity) AS 'Total Quantity'
+FROM gold.fact_sales;
+
+--Find the average selling price
+SELECT AVG(price)
+FROM gold.fact_sales;
+
+--Find the total number of orders
+SELECT COUNT(order_number) AS 'Total Orders'
+FROM gold.fact_sales;
+
+SELECT COUNT(DISTINCT order_number) AS 'Total Orders'
+FROM gold.fact_sales;
+
+-- Find the total number of products
+SELECT COUNT(product_id)
+FROM gold.dim_products;
+
+--Find the total number of customers
+SELECT COUNT(customer_id)
+FROM gold.dim_customers;
+
+--Find total number of customers that has placed an order
+SELECT COUNT(DISTINCT customer_key) AS 'Total Customers'
+FROM gold.fact_sales;
+
+
+--Generate a report that shows all key metrics of the business
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Nr. Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL 
+SELECT 'Total Nr. Products', COUNT(product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Nr Customers',COUNT(customer_key) FROM gold.dim_customers
