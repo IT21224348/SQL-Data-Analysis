6b70315c84b5b858ca14ebac554fed6aa0a233e5
Ravindu Sulakkana <99631317+IT21224348@users.noreply.github.com>
2025-03-20 23:37:44 +0530

    Update project2.sql

--- Project 1/project2.sql
+++ Project 1/project2.sql
@@ -7,3 +7,43 @@ FROM gold.fact_sales
 WHERE order_date  IS NOT NULL
 GROUP BY YEAR(order_date)
 ORDER BY YEAR(order_date) DESC;
+
+--Find the total sales of each month
+SELECT MONTH(order_date) AS Order_year,
+       SUM(sales_amount) AS Total_sales,
+	   COUNT(DISTINCT customer_key) AS Total_Customers,
+	   SUM(quantity) AS Total_Quantity
+FROM gold.fact_sales  
+WHERE order_date IS NOT NULL
+GROUP BY MONTH(order_date)
+ORDER BY MONTH(order_date)
+
+--Find total sales of each year each months
+SELECT YEAR(order_date) AS Order_year,
+       MONTH(order_date) AS Order_month,
+       SUM(sales_amount) AS Total_sales,
+	   COUNT(DISTINCT customer_key) AS Total_customers,
+	   SUM(quantity) AS Total_Quantity
+FROM gold.fact_sales  
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date),MONTH(order_date)
+ORDER BY YEAR(order_date),MONTH(order_date)
+
+SELECT DATETRUNC(MONTH,order_date) AS Order_year,
+       SUM(sales_amount) AS Total_sales,
+	   COUNT(DISTINCT customer_key) AS Total_customers,
+	   SUM(quantity) AS Total_Quantity
+FROM gold.fact_sales  
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH,order_date)
+ORDER BY DATETRUNC(MONTH,order_date)
+
+--Custom Format
+SELECT FORMAT(order_date, 'YYYY-MMM') AS Order_month,
+       SUM(sales_amount) AS Total_sales,
+	   COUNT(DISTINCT customer_key) AS Total_customer,
+	   SUM(quantity) AS Total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'YYYY-MMM')
+ORDER BY FORMAT(order_date, 'YYYY-MMM')
