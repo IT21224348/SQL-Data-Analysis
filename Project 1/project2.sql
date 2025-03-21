20e76997f597e6853f65f9a6c54af252eb7859eb
Ravindu Sulakkana <99631317+IT21224348@users.noreply.github.com>
2025-03-22 00:08:53 +0530

    Update project2.sql

--- Project 1/project2.sql
+++ Project 1/project2.sql
@@ -47,3 +47,18 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY FORMAT(order_date, 'YYYY-MMM')
 ORDER BY FORMAT(order_date, 'YYYY-MMM')
+
+--Calculate the total sales per month
+-- and the running total of sales over time
+
+SELECT 
+order_date,
+Total_sales,
+SUM(Total_sales) OVER (ORDER BY order_date)  AS running_total_sales
+FROM
+(
+SELECT DATETRUNC(MONTH,order_date) AS order_date,
+SUM(sales_amount) AS Total_sales
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH,order_date)) t
