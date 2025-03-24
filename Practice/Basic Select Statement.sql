78c59af23c87694088290d783166a9b906ca3325
Ravindu Sulakkana <99631317+IT21224348@users.noreply.github.com>
2025-03-24 23:28:04 +0530

    Add files via upload

---
+++ Practice/Basic Select Statement.sql
@@ -0,0 +1,16 @@
+--Basic select statement
+
+SELECT * 
+FROM dbo.customers;
+
+SELECT *
+FROM dbo.employees;
+
+SELECT *
+FROM dbo.orders;
+
+SELECT first_name,country
+FROM dbo.customers;
+
+SELECT emp_id, first_name, salary
+FROM dbo.employees
